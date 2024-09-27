343545fc0eb3f3974c38d8ff67c51436c7267ff2
Ivan Solopenko <solopenko16@gmail.com>
2024-09-27 17:55:50 +0300

    make build

---
+++ index.4f3f07bb.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth,l=e.clientHeight,o=t.offsetWidth,i=t.offsetHeight;t.style.top="".concat((l-i)/2,"px"),t.style.left="".concat((c-o)/2,"px");
+//# sourceMappingURL=index.4f3f07bb.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+const spiderTopPos = (wallHeight - spiderHeight) / 2;
+const spiderLeftPos = (wallWidth - spiderWidth) / 2;
+spider.style.top = `${spiderTopPos}px`;
+spider.style.left = `${spiderLeftPos}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
